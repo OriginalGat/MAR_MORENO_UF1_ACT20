34738225dce19b5be853342dbd7cd293189b1af4
INFO1-EQUIP12\Tarda <a23marmorgal@inspedralbes.cat>
2025-02-03 17:22:35 +0100

    Comentario para subir todos los archivos

--- app/src/main/java/com/example/actividad20/MainActivity.kt
+++ app/src/main/java/com/example/actividad20/MainActivity.kt
@@ -4,16 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
     private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,25 +22,26 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.buttonLoad)
 
         button.setOnClickListener {
-            runDownload()
-        }
-
-
-    }
-
-    private fun runDownload() {
-        textView.text = getString(R.string.string_descargando)
-        button.isEnabled = false
-
-        GlobalScope.launch(Dispatchers.IO) {
-            val downloadTime = (2000..5000).random().toLong()
-            delay(downloadTime)
-
-            withContext(Dispatchers.Main) {
-                textView.text = getString(R.string.descargado_correctamente)
-                button.isEnabled = true
+            CoroutineScope(Dispatchers.Main).launch {
+                awaitDownload()
             }
         }
     }
 
+    private suspend fun awaitDownload() {
+        textView.text = getString(R.string.string_descargando)
+        button.isEnabled = false
+
+        simulateDownload()
+
+        textView.text = getString(R.string.descargado_correctamente)
+        button.isEnabled = true
+    }
+
+    private suspend fun simulateDownload() {
+        val downloadTime = Random.nextLong(2000, 5000)
+        withContext(Dispatchers.IO) {
+            kotlinx.coroutines.delay(downloadTime)
+        }
+    }
 }
